5a8acb5fdffd90cee13a9fbd2e9d20c895d9005e
samantha-eva <samanthathiebaut@gmail.com>
2024-06-04 02:09:55 +0200

    creation des fichier react pour la partie client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { Routes, Route, Outlet } from "react-router-dom";
+import {
+  BlogDetails,
+  CategoriesPage,
+  Home,
+  LoginPage,
+  SignupPage,
+  WriterPage,
+} from "./pages"
+
+function Layout() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full flex flex-col min-h-screen px-4 md:px-10 2xl:px-28'>
+      {/* <Navbar /> */}
+      <div className='flex-1'>
+        <Outlet />
+      </div>
+      {/* <Footer /> */}
     </div>
   );
+}
+function App() {
+  
+  const theme = "light";
+
+  return (
+    <main className={theme}>
+      <div className={`w-full min-h-sreen relative  bg-white dark:bg-[#020b19]`}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/category' element={<CategoriesPage />} />
+            <Route path='/:slug/:id?' element={<BlogDetails />} />
+            <Route path='/writer/:id' element={<WriterPage />} />
+          </Route>
+
+          <Route path='/sign-up' element={<SignupPage />} />
+          <Route path='/sign-in' element={<LoginPage />} />
+
+        </Routes>
+      </div>
+    </main>
+  );
+
 }
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx}"],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
